0815354a6bb431aca7dec383dce861301e927cd5
Laura Neves <lauratneves@gmail.com>
2022-08-19 16:14:01 -0300

    feat: creating client model

---
+++ src/domain/clients/clients.model.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Model } from "sequelize";
+import { v4 as uuidv4 } from 'uuid';
+import sequelize from "../../infrastructure/database";
+import { Client } from "./client.entity";
+
+class ClientsModel extends Model<Client> {}
+
+ClientsModel.init({
+    id: {
+        type: DataTypes.STRING(36),
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: uuidv4()
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cpf: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique:true
+    }
+}, {
+    tableName: "clients",
+    timestamps: true,
+    underscored: true,
+    sequelize: sequelize
+})
+
+export default ClientsModel;
