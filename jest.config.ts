961f165de2b75c81204c47957595fa3295eba86e
Laura Neves <lauratneves@gmail.com>
2022-08-22 09:43:32 -0300

    feat(tests): adjusting configs to enable tests

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { InitialOptionsTsJest } from 'ts-jest';
+
+const config: InitialOptionsTsJest = {
+  modulePathIgnorePatterns: [
+    './dist'
+  ],
+  globals: {
+    'ts-jest': { }
+  }
+}
+
+export default config; 
